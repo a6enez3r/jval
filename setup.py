5d3c3025d98ff4ccaab582b61472e497650886b7
abenezer <abenezer@abenezers-MacBook-Air.local>
2021-12-10 09:26:20 -0800

    use make in workflows

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
+"""
+jval
+-------------
+
+validate a JSON object against a schema
+"""
 from setuptools import setup, find_packages
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-      name='jval',
-      version='0.0.1',
-      description='validate a JSON object against a schema',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='http://github.com/abmamo/jval',
-      author='Abenezer Mamo',
-      author_email='contact@abmamo.com',
-      license='MIT',
-      packages=find_packages(exclude=("tests",)),
-      zip_safe=False
+    name="jval",
+    version="0.0.1",
+    description="validate a JSON object against a schema",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/abmamo/jval",
+    author="Abenezer Mamo",
+    author_email="contact@abmamo.com",
+    license="MIT",
+    packages=find_packages(exclude=("tests",)),
+    zip_safe=False,
 )
