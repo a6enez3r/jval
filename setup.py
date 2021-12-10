ef806b3edecc265490aa6ccb7f4bd41c2ec9c05b
abenezer <abenezer@abenezers-MacBook-Air.local>
2021-12-10 09:23:48 -0800

    use make in workflows

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 from setuptools import setup, find_packages
-
-# read the contents of README file
 from os import path
-# get current file directory
+
 this_directory = path.abspath(path.dirname(__file__))
-# open README with UTF-8 encoding
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    # read README
     long_description = f.read()
 
 setup(
       name='jval',
       version='0.0.1',
-      description='recursively validate a given JSON object against a schema',
+      description='validate a JSON object against a schema',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      url='http://github.com/abmamo/validateJSON',
+      url='http://github.com/abmamo/jval',
       author='Abenezer Mamo',
       author_email='contact@abmamo.com',
       license='MIT',
